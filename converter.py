da2ea82c5fe46d79bae02effebc63c4847162f10
Nuwan-alt <nuwanm.19@cse.mrt.ac.lk>
2023-10-25 14:03:30 +0530

    updated

--- converter.py
+++ converter.py
@@ -6,17 +6,15 @@ import chardet
 with open('190394R - CS4642.xlsx', 'rb') as excel_file:
     result = chardet.detect(excel_file.read())
 
-# Convert the detected encoding to a codec
 encoding = result['encoding']
 
-# Read the Excel file using openpyxl with the detected encoding
 df = pd.read_excel('190394R - CS4642.xlsx', engine='openpyxl', dtype=str)
 
-# Convert the DataFrame to a list of dictionaries
+df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+
 data_list = df.to_dict(orient='records')
 
-# Write the data to a JSON file
-with open('dataset/data.json', 'w', encoding='utf-8') as json_file:
+with open('dataset/data1.json', 'w', encoding='utf-8') as json_file:
     json.dump(data_list, json_file, ensure_ascii=False, indent=4)
 
 print("Data has been successfully written to data.json")
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from ui import get_search_results_table
 def take_inputs():
     mode = int(input('Enter search mode: '))
 
-    if(mode == 0 or mode ==10):
+    if mode == 0 or mode == 10 or mode == -1:
         search_term = "kk"
     else:
         search_term = input('Enter search query: ')
@@ -52,6 +52,16 @@ def main():
         print(modes_text)
         mode, search_term = take_inputs()
         if mode < 0:
+            file_path_1 = 'thank.txt'  # Replace with the path to your file
+            try:
+                with open(file_path_1, 'r') as file:
+                    file_contents = file.read()
+                    print(file_contents)
+            except FileNotFoundError:
+                print(f"File '{file_path}' not found.")
+            except Exception as e:
+                print(f"An error occurred: {str(e)}")
+            modes_text = open('search_modes.txt', 'r').read()
             break
 
         results = multi_search(search_term, mode)
